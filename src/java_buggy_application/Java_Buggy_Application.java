95eecb9e8487c5833eb3902b0a7095dd9de00d6b
Joshua Kissoon <JoshuaKissoon@gmail.com>
2014-01-01 13:40:06 +0530

    Initial Commit

---
+++ src/java_buggy_application/Java_Buggy_Application.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package java_buggy_application;
+
+/**
+ *
+ * @author Joshua
+ */
+public class Java_Buggy_Application
+{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+        // TODO code application logic here
+    }
+    
+}
